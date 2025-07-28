fd31b261a571ed4bc9ff401b16a3b468e6d5bb69
sudoPierre <pierre.galliot.pg@outlook.fr>
2025-07-28 13:42:27 +0200

    Refactor configuration handling, update logging mechanism, and enhance conversation history management

--- main.py
+++ main.py
@@ -12,74 +12,81 @@ API_KEY = os.getenv("LLM_API_KEY")
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
-first_message_context = config["first_message_context"]
 context = config["context"]
 model = config["model"]
+base_url = config["base_url"]
+rules = "\n".join(config["rules"])
+auth_numbers = config["auth_numbers"]
 
 # Initialize the AI client
-client = OpenAI(api_key=API_KEY, base_url="https://api.perplexity.ai")
+client = OpenAI(api_key=API_KEY, base_url=base_url)
 path = os.path.dirname(os.path.abspath(__file__))
 _done = False
 
 # Function to create a log entry
 def log_entry(status, content):
-    completePath = os.path.join(path, "log.txt")
-    file = open(completePath, "r")
-    currentContent = file.read()
-    file.close()
-    file = open(completePath, "w")
-    file.write(f"{datetime.datetime.now()} - {status} : {content}\n{currentContent}")
-    file.close()
+    completePath = os.path.join(path, "main.log")
+    with open(completePath, "r") as file:
+        currentContent = file.read()
+    with open(completePath, "w") as file:
+        file.write(f"{datetime.datetime.now()} - {status} : {content}\n{currentContent}")
 
 # Function to edit the history of a conversation
-def edit_history(number, message):
-    completePath = os.path.join(path, "conv_history/", f"{number}.txt")
+def edit_history(number, date, role, message):
+    completePath = os.path.join(path, "conv_history/", f"{number}.json")
+    entry = {
+        "role": role,
+        "content": f" [{date.strftime("%Y-%m-%d %H:%M:%S")}] {message}"
+    }
     if os.path.isfile(completePath):
-        file = open(completePath, "a")
-        file.write(f"{message}\n")
-        log_entry("History", f"History of {number} edited")
+        with open(completePath, "r") as file:
+            history = json.load(file)
     else:
-        file = open(completePath, "x")
-        file.write(f"As a French SMS chatbot, continue the conversation naturally using this history:\n")
-        log_entry("History", f"History of {number} created")
-    file.close()
+        history = {
+            "history": []
+        }
+    history["history"].append(entry)
+    with open(completePath, "w") as file:
+        json.dump(history, file, indent=4)
+        file.close()
 
 # Function to get the history of a conversation
 def get_history(number):
-    completePath = os.path.join(path, "conv_history/", f"{number}.txt")
+    completePath = os.path.join(path, "conv_history/", f"{number}.json")
     if os.path.isfile(completePath):
-        file = open(completePath, "r")
-        output = file.read()
-        file.close()
-        return output
-    else:
-        file = open(completePath, "x")
-        file.write(f"As a French SMS chatbot, continue the conversation naturally using this history:\n")
-        file.close()
-        log_entry("History", f"History of {number} created")
-        return "You are a helpful assistant. Use only short french sentences with no formatting."
+        with open(completePath, "r") as file:
+            return json.load(file)["history"]
+    return []
 
 # Function to check if a number is authorized to chat
 def check_auth(number):
-    completePath = os.path.join(path, 'authContacts.csv')
-    with open(completePath, 'r') as file:
-        for line in file:
-            if number == line.strip():
-                return True
-        return False
+    for auth_number in auth_numbers:
+        if number == auth_number:
+            return True
+    return False
 
 # Function to ask the AI a question
 def ask_ai(number, message):
+    messages = [
+        {"role": "system",
+            "content": rules},
+        {"role": "system",
+            "content": f"Context: {context}"}
+    ]
+    
+    # Add history messages
+    messages.extend(get_history(number))
+    
+    # Add current user message
+    messages.append({
+        "role": "user",
+        "content": message
+    })
+    
     completion = client.chat.completions.create(
-            model="sonar",
-            messages=[
-                {"role": "system", "content": f"{get_history(number)} \n **Rules:** \n 1. Respond **only in French** with short, casual replies (1-2 sentences max) \n 2. No formatting (bullet points/emoji/markdown) \n 3. Mirror the user's tone (formal/informal) from the history \n 4. If context is unclear, ask 1 very brief clarifying question \n 5. Never repeat previous answers or greetings \n\n Example of acceptable response:  'Oui, je me souviens de notre discussion sur ça. Tu voulais que je te partage des exemples concrets ?'"},
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ]
-        )
+        model=model,
+        messages=messages
+    )
     return completion.choices[0].message
 
 # Function to send an SMS
@@ -102,8 +109,8 @@ while not _done :
     # check if the number is authorized to chat
     if check_auth(number):
         response = ask_ai(number, message).content
-        edit_history(number, f"[{datetime.datetime.now()}] User: {message}")
-        edit_history(number, f"[{datetime.datetime.now()}] Bot: {response}")
+        edit_history(number, datetime.datetime.now(), "user", message)
+        edit_history(number, datetime.datetime.now(), "assistant", response)
         log_entry("Success", f"Successful exchange with {number}")
         sens_sms(number, response)
     # if the number is not authorized, send a message to the user
